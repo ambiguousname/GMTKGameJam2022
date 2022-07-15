5f96b73d2b26cee9198241d5feb330db2bdcad81
ambiguousname <tyly04@gmail.com>
2022-07-15 13:13:58 -0700

    Added movement and temporary cursor replacement

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -1,21 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
     public float acceleration = 10.0f;
-    public float drag = 0.99f;
+    public float mouseSensitivity = 1.0f;
+
+    // TODO: Replace with something else (custom function calls?)
+    public Texture2D cursorToUse;
+
+    private Vector2 _accelIntent;
+    private Rigidbody2D _rigidbody;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        Cursor.lockState = CursorLockMode.Confined;
+        // TODO: Replace?
+        Cursor.SetCursor(cursorToUse, Vector2.zero, CursorMode.Auto);
+        Application.targetFrameRate = 60;
+        _rigidbody = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        
+        _rigidbody.AddForce(_accelIntent * acceleration);
     }
+
+    // All the events given from inputs. Set by Player Input component. Uses Unity Events.
+    #region InputEvents
+    public void OnMove(InputValue value) {
+        _accelIntent = value.Get<Vector2>();
+    }
+
+    #endregion
 }
