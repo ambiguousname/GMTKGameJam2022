215a0bd05cf1e3d72f5c64409c131a823c23d41b
NikThomas9 <nikrthomas@gmail.com>
2022-07-16 18:50:23 -0700

    Game Over Screen Implemented

--- Assets/GameOver.cs
+++ Assets/GameOver.cs
@@ -3,9 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class Credits : MonoBehaviour
+public class GameOver : MonoBehaviour
 {
-    public void ToMainMenu()
+    public void Continue()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void ToMenu()
     {
         SceneManager.LoadScene(0);
     }
